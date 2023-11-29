6653af3a5913e2c818a36e23e39095a6d6cfd455
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-11-29 19:58:10 +0100

    router

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import { Routes, Route, Navigate } from "react-router-dom";
+import { LoginPage } from "../auth";
+import { CalendarPage } from "../calendar";
+import { getEnvVariables } from "../helpers/getEnvVariables"
+import { RegisterPage } from "../auth/pages/register/RegisterPage";
+import { useAuthStore } from "../hooks";
+import { useEffect } from "react";
+
+export const AppRouter = () => {
+
+    const authStatus = "not-authenticated";
+
+    const { status, checkAuthToken } = useAuthStore();
+
+    useEffect(() => {
+        checkAuthToken();
+    }, [])
+
+    if (status === "checking") {
+
+        return (<h3>Cargando...</h3>)
+    }
+    return (
+
+        <Routes>
+            {
+                (status == "not-authenticated")
+                    ? (
+                        <>
+                            <Route path="/auth/*" element={<LoginPage />} />
+                            <Route path="/*" element={<Navigate to="/auth/login" />} />
+                        </>
+
+                    )
+                    : (
+                        <>
+                            <Route path="/" element={<CalendarPage />} />
+                            <Route path="/*" element={<Navigate to="/" />} />
+                        </>
+
+                    )
+            }
+
+            <Route path="/auth/register" element={<RegisterPage />} />
+
+        </Routes>
+    )
+}
