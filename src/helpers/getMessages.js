9352c11ce2da71659a9b1d5f480c854da500cfee
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-11-29 19:57:52 +0100

    helpers

---
+++ src/helpers/getMessages.js
@@ -0,0 +1,17 @@
+export const calendarMessagesEs = () => {
+    return {
+        allDay: 'Todo el día',
+        previous: '<',
+        next: '>',
+        today: 'Hoy',
+        month: 'Mes',
+        week: 'Semana',
+        day: 'Día',
+        agenda: 'Agenda',
+        date: 'Fecha',
+        time: 'Hora',
+        event: 'Citas',
+        noEventsInRange: 'No existen citas registradas',
+        showMore: total => `+ Ver más (${total})`
+    }
+};
---
+++ src/helpers/index.js
@@ -0,0 +1,11 @@
+import { localizer } from "./calendarLocalizer";
+import { convertEventsToDate } from "./convertEventsToDate";
+import { getEnvVariables } from "./getEnvVariables";
+import { calendarMessagesEs } from "./getMessages";
+export {
+    calendarMessagesEs,
+    localizer,
+    getEnvVariables,
+    convertEventsToDate
+
+}
